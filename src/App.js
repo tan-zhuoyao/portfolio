5173172a5b8e9422cefdadbf417fc8bade7d3548
Tan Zhuo Yao <e0406711@u.nus.edu>
2022-05-27 22:34:29 +0800

    add more breakpoints for background image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import FrontPage from './components/frontpage';
+import About from './components/about';
 
 function App() {
   return (
     <div class="relative">
       <FrontPage />
+      <About />
     </div>
   );
 }
