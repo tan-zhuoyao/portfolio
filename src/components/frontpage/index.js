89fb35d6d5b1fa59574f7dba7436ff4c54a6095d
Tan Zhuo Yao <e0406711@u.nus.edu>
2022-05-27 19:39:48 +0800

    add tailwind and navbar

---
+++ src/components/frontpage/index.js
@@ -0,0 +1,8 @@
+const FrontPage = () => {
+  return (
+    <div className="h-44 bg-blue-90">
+    </div>
+  )
+};
+
+export default FrontPage;
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.js",
+  ],
+  theme: {
+    screens: {
+      'mobile': { 'max': '640px' },
+      'tablet': { 'max': '768px' },
+    },
+    extend: {},
+  },
+  plugins: [],
+}
