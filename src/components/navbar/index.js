63ac572e60dc6a823f9c8c19e356cc31a706b990
Tan Zhuo Yao <e0406711@u.nus.edu>
2022-05-27 22:52:01 +0800

    add mobile menu icon

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,4 +1,8 @@
 const NavBar = () => {
+  const showMenu = () => {
+    console.log("clicked");
+  };
+
   return (
     <div class="text-5xl bg-black h-16 text-white mx-auto px-8 py-2 fixed top-0 z-50 w-screen flex justify-between">
       <div class="font-sans text-4xl font-bold">
@@ -29,6 +33,18 @@ const NavBar = () => {
         >
           Contact Me
         </a>
+      </div>
+      <div class="hidden mobile:flex">
+        <button class="outline-none mobile-menu-button" onClick={showMenu}>
+          <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M4 6h16M4 12h16M4 18h16"
+            />
+          </svg>
+        </button>
       </div>
     </div>
   );
