e864c4ec1f62e5b0feb91d0bba185dc250bf01ca
Tan Zhuo Yao <e0406711@u.nus.edu>
2022-05-27 22:06:28 +0800

    add animation to my name and change favicon

--- src/components/welcome/index.js
+++ src/components/welcome/index.js
@@ -1,15 +1,26 @@
+import ReactRevealText from 'react-reveal-text/lib/ReactRevealText';
+import { useState, useEffect } from 'react';
 import './welcome.css';
 
 const Welcome = () => {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    setShow(true);
+  }, []);
+
   return (
     <div class="h-full text-white">
       <div class="welcome h-full bg-no-repeat bg-cover" >
         <div class="h-16"></div>
-        <div class="font-sans text-6xl font-extrabold text-black ml-10 mt-10 mobile:ml-5 mobile:mt-5 mobile:text-4xl">
+        <div class="font-sans text-6xl font-extrabold text-black ml-10 mt-10 mobile:ml-5 mobile:mt-10 mobile:text-4xl">
           Hello World,
         </div>
         <div class="font-sans text-4xl font-bold text-black ml-10 mobile:ml-5 mobile:text-3xl">
           my name is
+        </div>
+        <div class="font-sans text-8xl font-extrabold text-black ml-10 mt-2 mobile:ml-5 mobile:mt-1 mobile:text-5xl">
+          <ReactRevealText show={show}>Zhuo Yao</ReactRevealText>
         </div>
       </div>
     </div >
