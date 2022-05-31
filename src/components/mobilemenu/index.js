4f4b72ab04252ef1ba22e5d7305e2a9db7b7bf16
Tan Zhuo Yao <e0406711@u.nus.edu>
2022-05-31 14:45:52 +0800

    add mobile menu

---
+++ src/components/mobilemenu/index.js
@@ -0,0 +1,40 @@
+const MobileMenu = () => {
+  return (
+    <div class="flex-col space-y-4">
+      <div class="text-center">
+        <a
+          href="/"
+          class="text-4xl"
+        >
+          About
+        </a>
+      </div>
+      <div class="text-center">
+        <a
+          href="/"
+          class="text-4xl"
+        >
+          Projects
+        </a>
+      </div>
+      <div class="text-center">
+        <a
+          href="/"
+          class="text-4xl"
+        >
+          Education
+        </a>
+      </div>
+      <div class="text-center">
+        <a
+          href="/"
+          class="text-4xl"
+        >
+          Contact Me
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default MobileMenu;
--- src/components/welcome/index.js
+++ src/components/welcome/index.js
@@ -3,24 +3,24 @@ import { useState, useEffect } from 'react';
 import './welcome.css';
 
 const Welcome = () => {
-  const [show, setShow] = useState(false);
+  const [showName, setShowName] = useState(false);
 
   useEffect(() => {
-    setShow(true);
-  }, [show]);
+    setShowName(true);
+  }, [showName]);
 
   return (
     <div class="h-full text-white">
       <div class="welcome h-full bg-no-repeat bg-cover" >
         <div class="h-16"></div>
-        <div class="font-sans text-6xl font-extrabold text-black ml-10 mt-10 mobile:ml-8 mobile:mt-10 mobile:text-4xl">
+        <div class="font-sans text-6xl font-bold text-black ml-10 mt-10 mobile:ml-8 mobile:mt-10 mobile:text-4xl">
           Hello World,
         </div>
-        <div class="font-sans text-4xl font-bold text-black ml-10 mobile:ml-8 mobile:text-3xl">
+        <div class="font-sans text-6xl font-bold text-black ml-10 mobile:ml-8 mobile:text-4xl">
           my name is
         </div>
         <div class="font-sans text-8xl font-extrabold text-black ml-10 mt-2 mobile:ml-8 mobile:mt-1 mobile:text-5xl">
-          <ReactRevealText show={show}>Zhuo Yao</ReactRevealText>
+          <ReactRevealText show={showName}>Zhuo Yao</ReactRevealText>
         </div>
       </div>
     </div >
