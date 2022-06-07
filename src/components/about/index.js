29b1a51fd99716d6d607a24ddad06c72fc90f84d
Tan Zhuo Yao <e0406711@u.nus.edu>
2022-06-07 12:18:40 +0800

    add experience timeline

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -1,15 +1,15 @@
-import Photo from "../../assets/passport_photo.png"
+import Photo from "../../assets/about_photo.png"
 
 const About = () => {
   return (
     <div id="about" className="bg-black text-white font-sans flex-col">
       <div className="h-16"></div>
-      <div className="pl-10 sticky top-16 bg-black text-6xl font-bold">
+      <div className="pl-10 sticky top-16 bg-black text-5xl font-bold pb-2">
         ABOUT
       </div>
       <div className="flex tablet:flex-col">
-        <div className="w-1/2 tablet:w-screen p-20 tablet:pb-0">
-          <img className="w-3/4 m-auto mt-auto mb-auto" src={Photo} alt="" />
+        <div className="w-1/2 m-auto tablet:w-screen p-20 tablet:pb-0">
+          <img className="w-3/4 m-auto mt-auto mb-auto rounded-lg" src={Photo} alt="" />
         </div>
         <div className="w-1/2 grid place-content-center tablet:w-screen">
           <div className="text-2xl font-medium p-20">
--- src/components/frontpage/index.js
+++ src/components/frontpage/index.js
@@ -3,12 +3,14 @@ import About from "../about";
 import Projects from "../projects";
 import Education from "../education";
 import ContactMe from "../contact";
+import Experience from "../experience";
 
 const FrontPage = () => {
   return (
     <div className="bg-black h-screen">
       <Welcome />
       <About />
+      <Experience />
       <Projects />
       <Education />
       <ContactMe />
