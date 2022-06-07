64d2f359374764a4636b49b03458f444064cda02
Tan Zhuo Yao <e0406711@u.nus.edu>
2022-06-07 17:44:39 +0800

    add projects

--- src/components/experience/index.js
+++ src/components/experience/index.js
@@ -1,9 +1,9 @@
 import './experience.css';
-import { JPMC, UNC, NUS, Switcheo } from '../../assets';
+import { JPMC, UNC, NUS, Switcheo, JaneStreet } from '../../assets';
 
 const Experience = () => {
   return (
-    <div id="experience" className="bg-black text-white font-sans flex-col">
+    <div id="experience" className="bg-black text-white font-sans flex-col w-screen">
       <div className="h-16"></div>
       <div className="pl-10 sticky top-16 bg-black text-5xl font-bold pb-2 z-10">
         EXPERIENCE
@@ -47,7 +47,7 @@ const Experience = () => {
                 <li className='ml-9'>COMP550: Algorithms & Analysis (A)</li>
                 <li className='ml-9'>COMP562: Theory of Machine Learning (A)</li>
                 <li className='ml-9'>STOR565: Machine Learning (A)</li>
-                <li>Achieved GPA: 4.0/4.0</li>
+                <li>Achieved GPA: 4.00/4.00</li>
               </ul>
             </div>
 
@@ -66,12 +66,32 @@ const Experience = () => {
                 <li>Designed and implemented the persistence of off-chain trade and ordering data into various databases to reduce SQL calls and enhance speed.</li>
                 <li>Migrated and restructured Switcheo Tradehub's blockchain codebase to support the Cosmos Stargate upgrade, to optimise for cross-chain interoperability.</li>
                 <li>Conceptualised the development and deployment of Scilla Smart Contracts onto Zilliqa blockchain to facilitate minting and trading of 10,000 Non-Fungible Tokens (NFT). Tested thoroughly and prevented potential contract exploits. The contract handled $2.1 million in transactions over a span of 3 days.</li>
+                <li>Tech Stack: ReactJS, ExpressJS, NodeJS, PostgreSQL</li>
+                <li>Languages used: Go, TypeScript, Scilla (smart contract)</li>
               </ul>
             </div>
 
             <div id="content">
               <div className='flex'>
-                <img className="h-20 w-20 m-auto ml-0 mr-2" src={NUS} alt=""></img>
+                <img className="h-20 w-20 m-auto ml-0 mr-2" src={JaneStreet} alt=""></img>
+                <div>
+                  <div className='text-3xl font-medium mobile:text-2xl'>
+                    Jane Street Capital
+                  </div>
+                  <div className='text-xl font-medium mobile:text-lg'>Electronic Trading Competition Winner</div>
+                  <div className='text-xl font-medium mobile:text-lg'>Oct 2020</div>
+                </div>
+              </div>
+              <ul className='list-disc ml-6 mt-2'>
+                <li>Participated and won the Jane Street Electronic Trading Competition where my team had to design and implement trading algorithms to execute trades on a simulated market.
+                  We were tasked to trade bonds, stocks, ETFs and ADRs</li>
+                <li>Emerged overall 1st place with our team’s trading bot being the most profitable.</li>
+              </ul>
+            </div>
+
+            <div id="content">
+              <div className='flex'>
+                <img className='h-20 w-20 m-auto ml-0 mr-2' src={NUS} alt=''></img>
                 <div>
                   <div className='text-3xl font-medium mobile:text-2xl'>
                     National University of Singapore
@@ -80,9 +100,10 @@ const Experience = () => {
                   <div className='text-xl font-medium mobile:text-lg'>May 2019 - Present (Expected Graduation: May 2023)</div>
                 </div>
               </div>
-              <ul className="list-disc ml-6 mt-2">
+              <ul className='list-disc ml-6 mt-2'>
                 <li>Computer Science major with a minor in Statistics</li>
                 <li>Expecting to graduate with First Class Honors</li>
+                <li>Current CAP: 4.61/5.00</li>
               </ul>
             </div>
           </div>
