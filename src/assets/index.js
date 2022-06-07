5a13151ffe74391c9334b37528f649198a4e4ad5
Tan Zhuo Yao <e0406711@u.nus.edu>
2022-06-07 16:03:07 +0800

    add logo for experiences and additional details

---
+++ src/assets/index.js
@@ -0,0 +1,8 @@
+export const JPMC = require('./jpmc.jpeg');
+export const NUS = require('./nus.jpeg');
+export const Switcheo = require('./switcheo.png');
+export const UNC = require('./unc.jpeg');
+export const ProfileMini = require('./profile_mini.png');
+export const ProfileMobile = require('./profile_mobile.png');
+export const ProfileTablet = require('./profile_tablet.png');
+export const Profile = require('./profile.png');
--- src/components/experience/index.js
+++ src/components/experience/index.js
@@ -1,4 +1,5 @@
 import './experience.css';
+import { JPMC, UNC, NUS, Switcheo } from '../../assets';
 
 const Experience = () => {
   return (
@@ -13,33 +14,55 @@ const Experience = () => {
           <div id="section">
             <div id="bead"></div>
             <div id="content">
-              <div className='text-3xl font-medium'>JPMorgan Chase & Co.</div>
-              <div className='text-xl font-medium'>Summer Analyst (Software Engineering)</div>
-              <div className='text-xl font-medium'> June 2022 - Present</div>
-              <ul className="list-disc ml-3 mt-2">
+              <div className='flex'>
+                <img className="h-20 w-20 m-auto ml-0 mr-2" src={JPMC} alt=""></img>
+                <div>
+                  <div className='text-3xl font-medium mobile:text-2xl'>
+                    JPMorgan Chase & Co.
+                  </div>
+                  <div className='text-xl font-medium mobile:text-lg'>Summer Analyst (Software Engineering)</div>
+                  <div className='text-xl font-medium mobile:text-lg'> June 2022 - Present</div>
+                </div>
+              </div>
+              <ul className="list-disc ml-6 mt-2">
                 <li>Corporate & Investment Banking Division</li>
                 <li>Regulatory Reporting</li>
               </ul>
+
             </div>
 
             <div id="content">
-              <div className='text-3xl font-medium'>University of North Carolina Chapel Hill</div>
-              <div className='text-xl font-medium'>Exchange Student</div>
-              <div className='text-xl font-medium'> Jan 2022 - May 2022 (Spring '22)</div>
-              <ul className="list-disc ml-3 mt-2">
+              <div className='flex'>
+                <img className="h-20 w-20 m-auto ml-0 mr-2" src={UNC} alt=""></img>
+                <div>
+                  <div className='text-3xl font-medium mobile:text-2xl'>
+                    University of North Carolina Chapel Hill
+                  </div>
+                  <div className='text-xl font-medium mobile:text-lg'>Exchange Student</div>
+                  <div className='text-xl font-medium mobile:text-lg'>Jan 2022 - May 2022 (Spring '22)</div>
+                </div>
+              </div>
+              <ul className="list-disc ml-6 mt-2">
                 <li>Relevant Coursework taken:</li>
-                <li className='ml-6'>COMP550</li>
-                <li className='ml-6'>COMP562</li>
-                <li className='ml-6'>STOR565</li>
+                <li className='ml-9'>COMP550: Algorithms & Analysis (A)</li>
+                <li className='ml-9'>COMP562: Theory of Machine Learning (A)</li>
+                <li className='ml-9'>STOR565: Machine Learning (A)</li>
                 <li>Achieved GPA: 4.0/4.0</li>
               </ul>
             </div>
 
             <div id="content">
-              <div className='text-3xl font-medium'>Switcheo Labs</div>
-              <div className='text-xl font-medium'>Software Engineering Intern</div>
-              <div className='text-xl font-medium'> June 2021 - Dec 2021</div>
-              <ul className="list-disc ml-3 mt-2">
+              <div className='flex'>
+                <img className="h-20 w-20 m-auto ml-0 mr-2" src={Switcheo} alt=""></img>
+                <div>
+                  <div className='text-3xl font-medium mobile:text-2xl'>
+                    Switcheo Labs
+                  </div>
+                  <div className='text-xl font-medium mobile:text-lg'>Software Engineering Intern</div>
+                  <div className='text-xl font-medium mobile:text-lg'>June 2021 - Dec 2021</div>
+                </div>
+              </div>
+              <ul className="list-disc ml-6 mt-2">
                 <li>Designed and implemented the persistence of off-chain trade and ordering data into various databases to reduce SQL calls and enhance speed.</li>
                 <li>Migrated and restructured Switcheo Tradehub's blockchain codebase to support the Cosmos Stargate upgrade, to optimise for cross-chain interoperability.</li>
                 <li>Conceptualised the development and deployment of Scilla Smart Contracts onto Zilliqa blockchain to facilitate minting and trading of 10,000 Non-Fungible Tokens (NFT). Tested thoroughly and prevented potential contract exploits. The contract handled $2.1 million in transactions over a span of 3 days.</li>
@@ -47,10 +70,17 @@ const Experience = () => {
             </div>
 
             <div id="content">
-              <div className='text-3xl font-medium'>National University of Singapore</div>
-              <div className='text-xl font-medium'>Student</div>
-              <div className='text-xl font-medium'> May 2019 - Present (Expected Graduation: May 2023)</div>
-              <ul className="list-disc ml-3 mt-2">
+              <div className='flex'>
+                <img className="h-20 w-20 m-auto ml-0 mr-2" src={NUS} alt=""></img>
+                <div>
+                  <div className='text-3xl font-medium mobile:text-2xl'>
+                    National University of Singapore
+                  </div>
+                  <div className='text-xl font-medium mobile:text-lg'>Student</div>
+                  <div className='text-xl font-medium mobile:text-lg'>May 2019 - Present (Expected Graduation: May 2023)</div>
+                </div>
+              </div>
+              <ul className="list-disc ml-6 mt-2">
                 <li>Computer Science major with a minor in Statistics</li>
                 <li>Expecting to graduate with First Class Honors</li>
               </ul>
@@ -58,7 +88,7 @@ const Experience = () => {
           </div>
         </div>
       </div>
-    </div>
+    </div >
   );
 }
 
